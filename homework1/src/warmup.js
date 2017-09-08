39d76bf04c149d32dcb137174700910e25eec08f
Ben Davis <bdavis42@lion.lmu.edu>
2017-09-07 21:37:49 -0700

    added one fnc and changed default git line endings to LF

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+function change(amt) {
+  const result = [];
+  let remaining = amt;
+  [25, 10, 5, 1].forEach((coin) => {
+    result.push(Math.floor(remaining(coin)));
+    remaining %= coin;
+  });
+  return result;
+}
+
+module.exports = {
+  change
+};
