4f89693de214d33bf4e5e1f9a1d370c428dd17bb
Ben Davis <bdavis42@lion.lmu.edu>
2017-09-08 11:24:34 -0700

    added a few functions

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -9,6 +9,29 @@ function change(amt) {
   return result;
 }
 
+function stripQuotes(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charAt(i) !== '"' && str.charAt(i) !== '\'') {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
+
+function scramble(str) {
+  let result = '';
+  const charArr = str.split('');
+  for (let i = charArr.length; i > 0; i -= 1) {
+    const random = Math.floor(Math.random() * i);
+    [charArr[i - 1], charArr[random]] = [charArr[random], charArr[i - 1]];
+  }
+  for (let j = 0; j < charArr.length; j += 1) {
+    result += charArr[j];
+  }
+  return result;
+}
+
 module.exports = {
-  change,
+  change, stripQuotes, scramble,
 };
