b6374be2d99524d5e1f9c7e7d9f80bd6e6709669
Ben Davis <bdavis42@lion.lmu.edu>
2017-09-07 21:58:53 -0700

    finally fixed line wrap issue.  also completed one fnc for warmup.js as well as added mocha capability to .yml file

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,13 +1,14 @@
 function change(amt) {
+  if (amt < 0) {throw new RangeError()};
   const result = [];
-  let remaining = amt;
+  var remaining = amt;
   [25, 10, 5, 1].forEach((coin) => {
-    result.push(Math.floor(remaining(coin)));
+    result.push(Math.floor(remaining / coin));
     remaining %= coin;
   });
   return result;
 }
 
 module.exports = {
-  change
+  change,
 };
