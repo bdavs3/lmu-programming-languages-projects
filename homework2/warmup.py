7fa056058fa41ef116478a2dbd915d776fe3387f
Brian Joerger <joergerbrian@gmail.com>
2017-10-05 15:34:40 -0700

    added say function, fixed powers and strip_quotes, return 1 for unfinished functions

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -1,26 +1,56 @@
 # pragma pylint: disable=E0001
 
 def change(amount):
+    "non-empty docstring"
+    return 1
 
 def strip_quotes(string):
-    return string.strip('\'\"')
+    "non-empty docstring"
+    return string.replace('\'', '').replace('"', '')
 
 def scramble(string):
+    "non-empty docstring"
+    return 1
 
 def powers(base, limit):
-    current = base;
+    "non-empty docstring"
+    current = 1
     while current <= limit:
         yield current
         current *= base
 
 def triples():
+    "non-empty docstring"
+    return 1
 
-def say(string):
+def say(string1=None):
+    "non-empty docstring"
+
+    if string1 is None:
+        return ''
+    def _inner_say(string2=None):
+        """
+        if val is None we return _inner_adder.v
+        else we increment and return ourselves
+        """
+        if string2 is None:
+            return _inner_say.string1
+        _inner_say.string1 += ' ' + string2
+        return _inner_say
+    _inner_say.string1 = string1  # save value
+    return _inner_say
 
 def interleave():
+    "non-empty docstring"
+    return 1
 
 class Cylinder:
+    "non-empty docstring"
 
-def make_crypto_function():
+def make_crypto_functions():
+    "non-empty docstring"
+    return 1
 
 def random_name():
+    "non-empty docstring"
+    return 1
