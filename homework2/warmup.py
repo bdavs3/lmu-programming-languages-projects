0deb42e58cdde07b5f7db8cb235b7c44525eb1f1
Ben Davis <bdavis42@lion.lmu.edu>
2017-10-05 20:47:39 -0700

    added interleave function

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -56,8 +56,8 @@ def say(string1=None):
     _inner_say.string1 = string1  # save value
     return _inner_say
 
-def interleave():
-    return 1
+def interleave(a, *b):
+    return [a[0]] + interleave(list(b), *a[1:]) if len(a) > 0 else list(b)
 
 class Cylinder(object):
     def __init__(self, radius=1, height=1):
