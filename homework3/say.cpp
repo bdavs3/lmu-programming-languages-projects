f926cc29d85371d3eeca20fe006ad13a385fa46f
bdavs3 <bdavis42@lion.lmu.edu>
2017-11-14 22:50:44 -0800

    changed say

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -1,4 +1,14 @@
 #include <cassert>
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int main() {
+  assert(say() == "");
+  assert(say("Hi")() == "Hi");
+  assert(say("Hello, ")("world!") == "Hello, world!");
+  assert(say("Just")("saying")("hi")() == "Just saying hi");
+
+  cout << "All tests passed\n";
+}
