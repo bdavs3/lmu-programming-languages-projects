1c9a003010fcc7f1b3f9282f70f82162feebd54a
Brian Joerger <brianjoerger@Brians-MacBook-Pro.local>
2017-12-08 18:50:29 -0800

    finished daysBetween

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -12,19 +12,20 @@ sumOfCubesOfOdds : List Int -> Int
 sumOfCubesOfOdds numbers =
 
 
-import Date exposing ( fromString, fromTime, toTime)
-import Tuple exposing ( first, second )
+import Date.Extra.Create exposing ( dateFromFields, getTimezoneOffset )
+import Date.Extra.Duration exposing ( diffDays )
+import Date.Extra.Period as Period exposing ( add )
+import Date exposing ( fromString, fromTime )
 daysBetween dateString1 dateString2 : String -> String -> Int
 daysBetween =
-  dates = (\
-          dateString1\
-          |> fromString\
-          |> Result.withDefault (fromTime 0)\
-          |> toTime\
-          , dateString2\
-          |> fromString\
-          |> Result.withDefault (fromTime 0)\
-          |> toTime\
-          )
-  timeDiff = ( first dates ) - ( second dates ) |> abs
-  daysDiff = timeDiff / ( 6000 * 60 * 60 * 24 )
+
+  dateString1 = "2017-02-14"
+  dateString2 = "2016-12-25"
+
+  date1WithOffset = dateString1 |> fromString |> Result.withDefault (fromTime 0)
+  date1 = add Period.Minute (getTimezoneOffset date1WithOffset) date1WithOffset
+
+  date2WithOffset = dateString2 |> fromString |> Result.withDefault (fromTime 0)
+  date2 = add Period.Minute (getTimezoneOffset date2WithOffset) date2WithOffset
+
+  diffDays date1 date2 |> abs
